1b89154f514566438c94e7d406fbee7092f9216a
CagdasAltunay <235060008@toros.edu.tr>
2025-12-24 17:08:13 +0300

    Create Member.cs

---
+++ Member.cs
@@ -0,0 +1,16 @@
+namespace LibraryApp
+{
+    public class Member
+    {
+        public string Name { get; set; }
+        public int MemberId { get; set; }
+
+        public Member(string name, int memberId)
+        {
+            Name = name;
+            MemberId = memberId;
+        }
+    }
+}
+
+
