25f57750165060e6b04064463c71bea9ab50ada0
CagdasAltunay <235060008@toros.edu.tr>
2025-12-24 17:08:35 +0300

    Create Book.cs

---
+++ Book.cs
@@ -0,0 +1,16 @@
+namespace LibraryApp
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+            IsAvailable = true;
+        }
+    }
+}
