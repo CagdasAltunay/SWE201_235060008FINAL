cd77d8c80d6b68907073566f54dd03e922e9e889
CagdasAltunay <235060008@toros.edu.tr>
2026-06-06 21:58:52 +0200

    Update Book.cs

--- Book.cs
+++ Book.cs
@@ -5,7 +5,8 @@ namespace LibraryApp
         public string Title { get; set; }
         public string Author { get; set; }
         public bool IsAvailable { get; set; }
-
+        
+// Constructor for creating a new book object
         public Book(string title, string author)
         {
             Title = title;
